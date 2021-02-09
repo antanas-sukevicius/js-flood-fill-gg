155375f57dfd7bb8fc877a0f207213828bd15f7c
unknown <antanas.sukevicius@gmail.com>
2021-02-09 19:16:05 +0200

    clean code

--- main.js
+++ main.js
@@ -12,78 +12,43 @@ class Point {
 }
 
 picture = [
-    [1, 4, 4, 4],
-    [1, 4, 1, 4],
-    [4, 4, 4, 4],
-    [4, 2, 3, 2]]
+    [1, 4, 4, 4, 4, 1],
+    [1, 4, 1, 4, 4, 4],
+    [1, 4, 1, 4, 4, 3],
+    [1, 4, 1, 1, 4, 2],
+    [4, 4, 4, 4, 4, 4],
+    [4, 2, 3, 4, 4, 2]]
 
 
 // input
-StartPoint = new Point(3, 0)
+StartPoint = new Point(1, 1)
 startPointColor = StartPoint.colour(picture);
 fillColour = 0;
 
 Xlimit = picture[0].length
 Ylimit = picture.length
-
+loopDirections = [[0, 1], [0, -1], [1, 0], [-1, 0]]
 
 StartPoint.setColour(picture, fillColour)
-
 queue = [StartPoint]
 
-a = getQueueItems(queue)
+fillArea(queue)
 
-
-
-function getQueueItems(queue) {
+function fillArea(queue) {
 
     point = queue[0]
 
-    x = point.x - 1
-    y = point.y
+    for (i = 0; i < loopDirections.length; i++) {
+        x = point.x + loopDirections[i][0]
+        y = point.y + loopDirections[i][1]
 
-    if (validateBoarders(x, y)) {
-        tmpPoint = new Point(x, y)
+        if (validateBoarders(x, y)) {
+            tmpPoint = new Point(x, y)
 
-        if (checkPoint(tmpPoint)) {
-            tmpPoint.setColour(picture, fillColour)
-            queue.push(tmpPoint)
-        }
-    }
-
-
-    x = point.x + 1
-    y = point.y
-
-    if (validateBoarders(x, y)) {
-        tmpPoint = new Point(x, y)
-
-
-        if (checkPoint(tmpPoint)) {
-            tmpPoint.setColour(picture, fillColour)
-            queue.push(tmpPoint)
-        }
-    }
-
-    x = point.x
-    y = point.y - 1
-    if (validateBoarders(x, y)) {
-        tmpPoint = new Point(x, y)
-
-        if (checkPoint(tmpPoint)) {
-            tmpPoint.setColour(picture, fillColour)
-            queue.push(tmpPoint)
-        }
-    }
-
-    x = point.x
-    y = point.y + 1
-    if (validateBoarders(x, y)) {
-        tmpPoint = new Point(x, y)
-
-        if (checkPoint(tmpPoint)) {
-            tmpPoint.setColour(picture, fillColour)
-            queue.push(tmpPoint)
+            if (checkPoint(tmpPoint)) {
+                tmpPoint.setColour(picture, fillColour)
+                queue.push(tmpPoint)
+            }
         }
     }
 
@@ -91,10 +56,9 @@ function getQueueItems(queue) {
 
     if (queue.length == 0) { return 0 }
 
-    getQueueItems(queue)
+    fillArea(queue)
 
 }
-
 
 function checkPoint(tmpPoint) {
     if (tmpPoint.colour(picture) == startPointColor) return true
@@ -108,6 +72,4 @@ function validateBoarders(x, y) {
 }
 
 
-
 console.table(picture)
-//console.log(JSON.stringify(picture,null,2))
